9d76f859d8a0403543d6777f3dbfb4fca59c27dc
Iryna Loza <irynloza@gmail.com>
2020-10-06 13:20:13 -0700

    Complete snake_to_camel

--- case.py
+++ case.py
@@ -5,11 +5,25 @@ For example::
     >>> snake_to_camel("hi_balloonicorn")
     'hiBalloonicorn'
 
+    >>> snake_to_camel("hi_balloonicorn_we_ready_to_play")
+    'hiBalloonicornWeReadyToPlay'
+
 """
 
 
 def snake_to_camel(variable_name):
     """Given a variable name in snake_case, return camelCase of name."""
+
+    phrase = variable_name.split('_')
+    result = phrase[:1]
+
+    for word in phrase[1:]:
+        word = word.replace(word[0], word[0].upper())
+        result.append(word)
+    return ''.join(result)    
+
+
+
 
 
 if __name__ == '__main__':
